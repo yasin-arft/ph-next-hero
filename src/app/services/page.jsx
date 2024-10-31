e706517bfb53b0ffdbe5adc74fb0fa69a4f4194c
yasin-arft <yasinarftsocial@gmail.com>
2024-10-31 12:33:36 +0600

    services route created

---
+++ src/app/services/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ServicesPage = () => {
+  return (
+    <main className='container mx-auto p-10'>
+      <section>
+        <h2 className='text-4xl text-center font-bold'>This is services page</h2>
+      </section>
+    </main>
+  );
+};
+
+export default ServicesPage;
